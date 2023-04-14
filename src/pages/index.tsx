af82626c491f152582bc928a3381c8290071fac5
Misael Gaytan <gaytanmisael@gmail.com>
2023-04-13 21:57:36 -0700

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,9 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
   return (
     <>
       <Head>
@@ -20,7 +20,12 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>
           {!user.isSignedIn && <SignInButton />}
-          {!!user.isSignedIn && <SignOutButton/>}
+          {!!user.isSignedIn && <SignOutButton />}
+        </div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
         </div>
       </main>
     </>
